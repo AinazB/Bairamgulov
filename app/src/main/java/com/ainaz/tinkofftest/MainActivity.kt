62806f950996fdcb916c68c659d5c2ce3e00eea8
abayramgulov <start_againS@mail.ru>
2022-02-05 18:55:14 +0300

    add fragment container in activity

--- app/src/main/java/com/ainaz/tinkofftest/MainActivity.kt
+++ app/src/main/java/com/ainaz/tinkofftest/MainActivity.kt
@@ -2,10 +2,17 @@ package com.ainaz.tinkofftest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import com.ainaz.tinkofftest.ui.container.ContainerFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        savedInstanceState?.let {
+            supportFragmentManager.commit {
+                add(R.id.container, ContainerFragment())
+            }
+        }
     }
 }
