c437ed8de03d78408bd5976b14fefd08dcb1401a
abayramgulov <start_againS@mail.ru>
2022-02-06 21:15:54 +0300

    add fragmentHot

--- app/src/main/java/com/ainaz/tinkofftest/ui/hot/HotFragment.kt
+++ app/src/main/java/com/ainaz/tinkofftest/ui/hot/HotFragment.kt
@@ -1,6 +1,52 @@
 package com.ainaz.tinkofftest.ui.hot
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.ainaz.tinkofftest.App
+import com.ainaz.tinkofftest.R
+import com.ainaz.tinkofftest.databinding.FragmentHotBinding
+import com.ainaz.tinkofftest.domain.usecase.GetHotGifsUseCase
+import com.ainaz.tinkofftest.ui.latest.GifsAdapter
+import kotlinx.coroutines.flow.collectLatest
+import javax.inject.Inject
 
-class HotFragment : Fragment() {
+class HotFragment : Fragment(R.layout.fragment_hot) {
+    private var _binding: FragmentHotBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var getHotGifs: GetHotGifsUseCase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity?.application as App).appComponent.inject(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHotBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupList()
+    }
+
+    private fun setupList() {
+        val adapter = GifsAdapter()
+        binding.hotRv.adapter = adapter
+        lifecycleScope.launchWhenStarted {
+            getHotGifs().collectLatest {
+                adapter.submitData(it)
+            }
+        }
+    }
 }
