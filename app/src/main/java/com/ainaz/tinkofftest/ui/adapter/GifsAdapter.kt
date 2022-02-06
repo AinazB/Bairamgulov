fb5dbf80e739f34f40eef5ac1b72f1ddc3785e87
abayramgulov <start_againS@mail.ru>
2022-02-06 22:14:25 +0300

    add shimmer effect

--- app/src/main/java/com/ainaz/tinkofftest/ui/adapter/GifsAdapter.kt
+++ app/src/main/java/com/ainaz/tinkofftest/ui/adapter/GifsAdapter.kt
@@ -6,9 +6,13 @@ import android.widget.ImageView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.ainaz.tinkofftest.R
 import com.ainaz.tinkofftest.databinding.GifListItemBinding
 import com.ainaz.tinkofftest.domain.model.Gif
 import com.bumptech.glide.Glide
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class GifsAdapter : PagingDataAdapter<Gif, GifsAdapter.Holder>(GifsDiffCallback()) {
 
@@ -32,7 +36,25 @@ class GifsAdapter : PagingDataAdapter<Gif, GifsAdapter.Holder>(GifsDiffCallback(
     private fun loadGif(imageView: ImageView, url: String) {
         Glide.with(imageView.context)
             .load(url)
+            .placeholder(setupShimmer())
             .into(imageView)
+    }
+
+    private fun setupShimmer(): ShimmerDrawable {
+        val shimmer =
+            Shimmer.AlphaHighlightBuilder()
+                .setDuration(1800)
+                .setBaseAlpha(0.7f)
+                .setHighlightAlpha(0.6f)
+                .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+                .setAutoStart(true)
+                .build()
+
+        val shimmerDrawable = ShimmerDrawable().apply {
+            setShimmer(shimmer)
+        }
+
+        return shimmerDrawable
     }
 }
 
